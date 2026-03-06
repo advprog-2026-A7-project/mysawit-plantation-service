6b750b11c3e19ab31f02136ccbbe04f87714eba6
FalahHadziqul <hadziqulfalah18@gmail.com>
2026-03-06 20:45:52 +0700

    fix : removed unused test

--- src/test/java/com/mysawit/plantation/controller/PlantationControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.mysawit.plantation.controller;
-
-/**
- * This class has been replaced by PlantationControllerWebMvcTest
- * It is intentionally left empty to fix compilation errors caused by the old outdated tests.
- */
-class PlantationControllerTest {
-}
