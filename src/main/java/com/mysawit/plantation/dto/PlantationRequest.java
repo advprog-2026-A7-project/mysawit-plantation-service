257c94e3d0d92434436d214b49c601f0ac1ed2aa
FalahHadziqul <hadziqulfalah18@gmail.com>
2026-05-27 09:29:20 +0700

    fix : sonar duplicate , extends right base class

--- src/main/java/com/mysawit/plantation/dto/PlantationRequest.java
+++ src/main/java/com/mysawit/plantation/dto/PlantationRequest.java
@@ -1,24 +1,4 @@
 package com.mysawit.plantation.dto;
 
-public class PlantationRequest extends BasePlantationRequest {
-
-    private String code;
-
-    private String ownerId;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(String ownerId) {
-        this.ownerId = ownerId;
-    }
+public class PlantationRequest extends CreatePlantationRequest {
 }
